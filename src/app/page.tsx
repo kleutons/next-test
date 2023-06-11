f588be08f651800a473d48daf56957be9298ff3c
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-06-11 12:02:57 -0300

    delete: page blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 
-import { useRouter } from 'next/navigation'
-
 export default function Home() {
-  const router = useRouter();
 
   return (
     <main className='main'>
       <h1> Home </h1>
       <p> Test Next </p>
-      <button onClick={() => router.push('/user?id=01')}> Click Page User 01 </button>
-      <button onClick={() => router.push('/user?id=02')}> Click Page User 02 </button>
     </main>
   )
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,27 +1,8 @@
 'use client'
-// export const dynamic = 'force-dynamic'
-
 import { useRouter } from 'next/navigation'
-
-// export default function User({searchParams}: {searchParams: { id: string}}){
-//     const router = useRouter();
-
-//     const idUser = searchParams.id;
-//     console.log(idUser);
-
-//     return (
-//          <main className='main'>
-//          <h1> Page User </h1>
-//          <p> User Get Id == <span className='id'> {idUser} </span> </p>
-//          <button onClick={() => router.push('./')}> Back </button>
-//        </main>
-//     )
-// }
-
 
 export default function User(){
   const router = useRouter();
-
   return (
        <main className='main'>
        <h1> Page User </h1>

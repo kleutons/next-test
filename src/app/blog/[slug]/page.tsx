e511d15f03c2c9387448ecd3a463df82b520847d
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-06-11 12:19:35 -0300

    add: force-dynamic

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-
+export const dynamic = 'force-dynamic';
 import { useRouter } from "next/navigation";
 
 export default function BlogSlug({ params }: { params: { slug: string } }){

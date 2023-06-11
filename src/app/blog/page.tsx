b60aadd52d5add75a0bd5ce01a09e08669c46c21
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-06-11 12:16:04 -0300

    add: Params Blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useRouter } from 'next/navigation'
+
+export default function Blog(){
+  const router = useRouter();
+  return (
+       <main className='main'>
+       <h1> Page Blog </h1>
+       <p> not found </p>
+       <button onClick={() => router.push('/01')}> id 01 </button>
+       <button onClick={() => router.push('/01')}> id 02 </button>
+       <button onClick={() => router.push('/')}> Back </button>
+     </main>
+  )
+}
